55b4f0a7791731194d98abed1874fe064169d314
Zac <kicksat@gmail.com>
2013-09-17 20:42:57 -0700

    Initial Commit - Asteroid 3D models, Energia code, 2D Real Time Plotter

---
+++ MagnetometerSerial/MagnetometerSerial.ino
@@ -0,0 +1,22 @@
+#include <SpriteMag.h>
+
+SpriteMag mag = SpriteMag();
+
+void setup() {
+  mag.init();
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  //Values are in Guass
+  MagneticField b = mag.read();
+  
+  Serial.print(b.x);
+  Serial.print("\t");
+  Serial.print(b.y);
+  Serial.print("\t");
+  Serial.println(b.z);
+  
+  delay(100);
+}
