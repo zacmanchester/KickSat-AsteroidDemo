ef91c0ee44e4f62f3df3694fd3bc74ad48ed9808
Zac <kicksat@gmail.com>
2013-10-22 03:35:34 -0700

    Added pygame version of plotframes

---
+++ plotframes2.py
@@ -0,0 +1,20 @@
+import pygame   #1
+from pygame.locals import *
+from sys import exit
+ 
+pygame.init()
+screen=pygame.display.set_mode((560,420),0,16)
+pygame.display.set_caption("Hello World")
+
+dtheta = 3
+frames = []
+for k in range(0, 360, dtheta):
+    filename = './Frames_560/Frame'+str(k)+'.png'
+    frames.append(pygame.image.load(filename).convert())
+
+for frame in frames:
+    screen.blit(frame,(0,0))
+    pygame.display.update()
+    
+exit()
+
