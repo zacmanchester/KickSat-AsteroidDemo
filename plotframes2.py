c662d7ad5c27457391aa8ba69a504f94eec84116
Zac <kicksat@gmail.com>
2013-10-22 05:55:36 -0700

    Changed to 800x600 frames

--- plotframes2.py
+++ plotframes2.py
@@ -3,13 +3,13 @@ from pygame.locals import *
 from sys import exit
  
 pygame.init()
-screen=pygame.display.set_mode((560,420),0,16)
-pygame.display.set_caption("Hello World")
+screen=pygame.display.set_mode((800,600),pygame.FULLSCREEN,16)
+pygame.display.set_caption("KickSat Asteroid Tracker")
 
 dtheta = 3
 frames = []
 for k in range(0, 360, dtheta):
-    filename = './Frames_560/Frame'+str(k)+'.png'
+    filename = './Frames_800/Frame'+str(k)+'.png'
     frames.append(pygame.image.load(filename).convert())
 
 for frame in frames:
